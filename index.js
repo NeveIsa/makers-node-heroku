e6f8cc86f392187923502332fc3d6add6f6d5d6b
sampad <sbmohant@usc.edu>
2021-04-21 18:38:24 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.get("/",(req,res) => {
+
+	console.log("GET /")
+	res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+		console.log('Started Server...')
+})
