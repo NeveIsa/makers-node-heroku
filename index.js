d0d588f5ebe22c923ca0e3419b6e3f613f2eb00a
sampad <sbmohant@usc.edu>
2021-04-21 19:03:38 -0700

    aded name

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express();
 
+// use json
+app.use(express.json())
+app.use(express.urlencoded({extended: true} ) );
+
 const PORT = process.env.PORT || 3000;
+
+
+const people = [];
 
 app.get("/",(req,res) => {
 
-	console.log("GET /")
-	res.send('Hello World')
+	res.json(people);
 })
+
+app.post("/insert", (req,res) =>{
+
+	const name = req.body.name;
+	
+	people.push(name)
+
+	res.sendStatus(200);
+} )
+
 
 app.listen(PORT, () => {
 		console.log('Started Server...')
